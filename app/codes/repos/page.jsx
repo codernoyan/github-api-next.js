d14c8c579c404dfd54d84ab162448c5d59a101dc
codernoyan <coder.noyan@gmail.com>
2023-04-23 01:47:57 +0600

    added a button to back to profile page

--- app/codes/repos/page.jsx
+++ app/codes/repos/page.jsx
@@ -10,7 +10,12 @@ export default async function Repos() {
   const repos = await getRepos('Preankasaha');
   return (
     <section className="px-2 mb-6">
-      <h2 className="text-xl font-bold mt-2">Repositories:</h2>
+      <div className="flex justify-between items-center">
+        <h2 className="text-xl font-bold mt-2">Repositories:</h2>
+        <div className="mt-2">
+          <Link href="/codes" className="bg-slate-100 font-semibold px-4 py-2 hover:bg-slate-900 hover:text-white transition-colors">Back to Profile</Link>
+        </div>
+      </div>
       {/* repos list */}
       <div className="mt-4">
         <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
